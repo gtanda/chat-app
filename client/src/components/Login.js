f0ddaab4f91495753ed11033fe980622e049ab54
gtanda <govind_tanda1@hotmail.com>
2023-02-04 20:10:05 -0800

    added clientside validation for login and registration

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,31 +1,47 @@
-import {Button, Flex, Heading, Input, Text} from "@chakra-ui/react";
+import {Button, Flex, FormControl, FormLabel, Heading, Input, Text, FormErrorMessage} from "@chakra-ui/react";
 import {useDispatch} from "react-redux";
 import {changeView} from "../features/indexView/indexView";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import authService from "../services/auth";
 
 const Login = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [isError, setIsError] = useState(false);
     const dispatch = useDispatch();
+    const timerId = useRef(null);
 
     const handleLogin = async (e) => {
         e.preventDefault();
+        const userCheck = username.replace(/\s/g, ''); // remove whitespace
+        if (!userCheck || !password) {
+            setIsError(true);
+            clearTimeout(timerId.current);
+            timerId.current = setTimeout(() => {
+                setIsError(false);
+            }, 5000);
+            return;
+        }
+
         const loginUser = await authService.login({username, password});
-        console.log(loginUser);
+        console.log('loginUser', loginUser);
     }
+
 
     return (
         <Flex height="100vh" alignItems="center" justifyContent="center">
             <Flex direction="column" shadow={"0 0 0.5em hsl(220deg 40% 40%)"} p={12} rounded={6}>
                 <Heading mb={6}>Login</Heading>
-                <Input placeholder={"Username"} variant={"filled"} mb={3} type={"input"}
-                       onChange={(e) => setUsername(e.target.value)}/>
-                <Input placeholder={"Password"} variant={"filled"} mb={6} type={"password"}
-                       onChange={(e) => setPassword(e.target.value)}/>
-                <Button onClick={handleLogin} colorScheme={"teal"} mb={3}>Login</Button>
-                <Text _hover={{cursor: "pointer"}} onClick={() => dispatch(changeView())} color={"blue.500"}>Not a
-                    User? <strong>Register</strong></Text>
+                <FormControl isInvalid={isError}>
+                    <Input placeholder={"Username"} variant={"filled"} mb={3} type={"input"}
+                           onChange={(e) => setUsername(e.target.value.trim())}/>
+                    <Input placeholder={"Password"} variant={"filled"} mb={6} type={"password"}
+                           onChange={(e) => setPassword(e.target.value.trim())}/>
+                    <Button onClick={handleLogin} colorScheme={"teal"} mb={3}>Login</Button>
+                    <Text _hover={{cursor: "pointer"}} onClick={() => dispatch(changeView())} color={"blue.500"}>Not a
+                        User? <strong>Register</strong></Text>
+                    {isError ? <FormErrorMessage><strong>Valid Username and Password required.</strong></FormErrorMessage>: null}
+                </FormControl>
             </Flex>
         </Flex>);
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,18 +1,29 @@
-import {Button, Flex, Heading, Input, Text} from "@chakra-ui/react";
+import {Button, Flex, FormControl, FormErrorMessage, Heading, Input, Text} from "@chakra-ui/react";
 import {useDispatch} from "react-redux";
 import {changeView} from "../features/indexView/indexView";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import authService from "../services/auth";
 import auth from "../services/auth";
 
 const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-
+    const [isError, setIsError] = useState(false);
     const dispatch = useDispatch();
+    const timerId = useRef(null);
 
     const handleRegister = async (e) => {
         e.preventDefault();
+        const userCheck = username.replace(/\s/g, ''); // remove whitespace
+        if (!userCheck || !password || !userCheck.match( /^[a-zA-Z0-9]+$/)|| !password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/)) {
+            setIsError(true);
+            clearTimeout(timerId.current);
+            timerId.current = setTimeout(() => {
+                setIsError(false);
+            }, 8000);
+            return;
+        }
+
         const registeredUser = await authService.register({username, password});
         console.log(registeredUser);
     }
@@ -21,13 +32,17 @@ const Register = () => {
         <Flex height="100vh" alignItems="center" justifyContent="center">
             <Flex direction="column" shadow={"0 0 0.5em hsl(220deg 40% 40%)"} p={12} rounded={6}>
                 <Heading mb={6}>Register</Heading>
-                <Input placeholder={"Username"} variant={"filled"} mb={3} type={"email"}
-                       onChange={(e) => setUsername(e.target.value)}/>
-                <Input placeholder={"Password"} variant={"filled"} mb={6} type={"password"}
-                       onChange={(e) => setPassword(e.target.value)}/>
-                <Button colorScheme={"teal"} mb={3} onClick={handleRegister}>Register</Button>
-                <Text _hover={{cursor: "pointer"}} onClick={() => dispatch(changeView())} color={"blue.500"}>Already a
-                    user? <strong>Login</strong></Text>
+                <FormControl isInvalid={isError}>
+                    <Input placeholder={"Username"} variant={"filled"} mb={3} type={"email"}
+                           onChange={(e) => setUsername(e.target.value.trim())}/>
+                    <Input placeholder={"Password"} variant={"filled"} mb={6} type={"password"}
+                           onChange={(e) => setPassword(e.target.value.trim())}/>
+                    <Button colorScheme={"teal"} mb={3} onClick={handleRegister}>Register</Button>
+                    <Text _hover={{cursor: "pointer"}} onClick={() => dispatch(changeView())} color={"blue.500"}>Already
+                        a
+                        user? <strong>Login</strong></Text>
+                    {isError ? <FormErrorMessage>Valid Username (<strong>Alphanumeric only</strong>) and Password (<strong>Must contain 1 digit, uppercase and lowercase character, and be between 6 to 20 characters</strong>) required.</FormErrorMessage> : null}
+                </FormControl>
             </Flex>
         </Flex>);
 }
